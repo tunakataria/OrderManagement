4208cc63809dbc141f689490c6e03b329fbdb85b
hitesh <katariah.hitesh@yahoo.in>
2020-03-20 10:48:39 -0700

    pushing stuff and aggregating all the micro-services

--- cart-service/src/main/java/com/birlasoft/cartservice/services/ICartService.java
+++ cart-service/src/main/java/com/birlasoft/cartservice/services/ICartService.java
@@ -2,7 +2,6 @@ package com.birlasoft.cartservice.services;
 
 import com.birlasoft.cartservice.context.UserServiceReqContext;
 import com.birlasoft.cartservice.model.CartResponse;
-import com.birlasoft.cartservice.model.CartUserContext;
 import com.birlasoft.cartservice.model.ProductRequest;
 
 public interface ICartService {
